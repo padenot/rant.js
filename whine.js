ec17438a304968d73fa70ae64e0a69d46551d038
Paul ADENOT <paul@paul.cx>
2011-10-03 17:37:33 +0200

    modif

--- whine.js
+++ whine.js
@@ -2,6 +2,13 @@ var fs = require('fs');
 var http = require('http');
 
 var comments = null;
+var saveTimeout = null;
+// [
+//    {client: client, data: "data reveived for now"},
+//    {client: client, data: "data reveived for now"},
+//    {client: client, data: "data reveived for now"},
+// ]
+var clients = [];
 var data = "";
 
 function init_comments() {
@@ -12,6 +19,7 @@ function init_comments() {
     comments = JSON.parse(data.toString());
     comments = index(comments);
     console.log(comments);
+    deindex(comments);
   });
 }
 
@@ -31,15 +39,37 @@ function index(comments) {
   return indexed;
 }
 
+function deindex(comments) {
+  var deindexed = [];
+  for (var i in comments) {
+    for (var j = 0; j < comments[i].length; j++) {
+      deindexed.push(comments[i][j]);
+      deindexed[deindexed.length-1].article = i;
+    }
+  }
+  console.log(deindexed);
+  return deindexed;
+}
+
 function get_comments(article) {
   return comments[article];
 }
 
 function get_article(request) {
-  return request.url.substr(1);
+  return request.url;
+}
+
+function saveComments() {
+  fs.writeFile(JSON.stringify(deindex(comments)), "comments.json", function (err) {
+    if (err) throw err;
+    console.log('comments.json saved');
+  });
 }
 
 function addComment(obj) {
+  if (saveTimeout != null) {
+    clearTimeout(saveTimeout);
+  }
   if (obj.author.length == 0 || obj.email.length == 0 || obj.content.length == 0) {
     return;
   }
@@ -49,15 +79,19 @@ function addComment(obj) {
     comments[article] = [];
   }
   comments[article].push(obj);
+  saveTimeout = setTimeout(saveComments, 500);
 }
 
 init_comments();
 http.createServer(function (request, response) {
   // Get comments
   if (request.method === "GET") {
+    console.log(request.url);
     var article = get_article(request);
     var comments = get_comments(article);
+    console.log(comments);
     var data = JSON.stringify(comments);
+    console.log(data);
     var content_length = 0;
     if (data) {
       content_length = data.length;
@@ -67,17 +101,28 @@ http.createServer(function (request, response) {
       "Content-Length": content_length});
     response.end(data);
   } else if (request.method === "POST") {
-    // problem if concurrent submission ?
+    console.log("POST");
     request.addListener("data", function(chunk) {
-      console.log("chunk");
-      data=chunk.toString("utf8");
-      console.log(data);
+      if (!client[request.connection]) {
+        client[request.connection] = "";
+      }
+      if (chunk) {
+        clients[request.connection] += chunk.toString("utf8");
+      }
+      console.log(chunk);
     });
     request.addListener("end", function() {
-      var obj = JSON.parse(data.toString("utf8"));
-      addComment(obj);
-      console.log(obj);
+      if (data) {
+        var obj = JSON.parse(data.toString("utf8"));
+        addComment(obj);
+        delete clients[request.connection];
+        console.log(obj);
+      } else {
+        console.log("data : " + data);
+      }
     });
+  } else {
+    console.log("rejected");
   }
 }).listen(8125);
 
